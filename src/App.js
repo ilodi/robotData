5e48d86b93bcdfce5fba314cc0a9309655ee9c94
Obsidian Soft <vega@Mac-mini-de-Obsidian.local>
2019-07-19 19:05:50 -0500

    estableciendo el carrucel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Hero from './components/Hero';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
 import Ai from './components/Ai';
@@ -9,6 +10,9 @@ import Advanced from './components/Advanced';
 import About from './components/About';
 
 // Static File
+import hero_One from './static/img/hero.jpg';
+import hero_Two from './static/img/heroOne.jpg';
+
 import logo from './static/brand/logo.svg';
 import bg_img_cont from './static/img/contact.jpg';
 import iconAi from './static/icon/aiLogo.svg';
@@ -19,17 +23,22 @@ import imgAuxParthners from './static/img/f.jpeg';
 import iconAboutA from './static/icon/aboutA.svg';
 import iconAboutB from './static/icon/aboutB.svg';
 
-
 function App() {
   const title = 'Robot Data Co. Ltd. is a Hong Kong AI company';
   const date = new Date().getFullYear();
   return (
     <div className="App">
       <Header />
+      <Hero hero_One={hero_One} hero_Two={hero_Two}/>
       <About logo={logo} iconAboutA={iconAboutA} iconAboutB={iconAboutB} />
       <Advanced />
       <Banner />
-      <Clients iconHK={iconHK} imgClients={imgClients} imgParthners={imgParthners} imgAuxParthners={imgAuxParthners} />
+      <Clients
+        iconHK={iconHK}
+        imgClients={imgClients}
+        imgParthners={imgParthners}
+        imgAuxParthners={imgAuxParthners}
+      />
       <Ai iconAi={iconAi} />
       <Contact bg_img_cont={bg_img_cont} />
       <Footer date={date} logo={logo} title={title} />
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
-
+import Slider from 'react-slick';
 class Hero extends Component {
   render() {
-    return <>
+    var settings = {
+      autoplay: true,
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <>
+        <div className="container-fluid">
+          <div className="s12 m8">
+            <Slider {...settings}>
+              <div>
+                <img
+                  className="heroImgCarrucel"
+                  src={this.props.hero_One}
+                  alt="Hero Robot"
+                />
+              </div>
+              <div>
+                <img
+                  className="heroImgCarrucel"
+                  src={this.props.hero_Two}
+                  alt="Robot data cover"
+                />
+              </div>
+            </Slider>
+          </div>
 
-    </>;
+          <div>
+
+            </div>
+        </div>
+      </>
+    );
   }
 }
 
